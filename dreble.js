bac3c2b9a58171173aab11a60afec626f0dad0dc
Mkenzo_8 <mespinsanz@gmail.com>
2019-03-17 17:22:35 +0100

    testing

---
+++ dreble.js
@@ -0,0 +1,266 @@
+window.onload = function(){
+	var style= document.createElement('script');
+  style.innerHTML = "";
+    document.body.appendChild(style); 
+	Main();
+}
+window.onerror = function(){
+	document.body.innerHTML = `
+	<p style="left:50%; transform: translate(-50%, -45%); top:50%; position:fixed;">An error ocurred.</p>
+	`;
+}
+var i = 0;
+function load(config){
+	
+	var act = config['defaultActivity'];
+	document.body.innerHTML += act.innerHTML;
+	activitiesHistory.push(act.children[0].id);
+	refreshRippleElements();
+
+}
+var activitiesHistory = [];
+class Navbar extends  HTMLElement {
+    constructor(activity,id,position,code) {
+    	super();
+    	
+    	if(activity!=undefined){ //Created from JavaScript
+    	var page = document.getElementById(activity).children[0];
+
+    	 page.innerHTML += "<div id="+id+" class='navbar "+position+"'>"+code+"</div>";	
+
+				        if(position==="top"){
+				        	page.style["padding-top"] = " 60px ";
+				        }else if(position==="bottom"){
+				        	page.style["padding-bottom"] = " 60px ";
+				        }
+				      }
+
+    }
+    connectedCallback(activity,id,position,code){  //Created from html
+  
+			var page = document.getElementById(this.getAttribute("activity")).children[0];
+			var position = this.getAttribute("position");
+			var code = this.innerHTML;
+			var id = this.getAttribute("id");
+			this.remove();
+
+			page.innerHTML += "<div id="+id+" class='navbar "+position+"'>"+code+"</div>";	
+				        if(position==="top"){
+				        	page.style["padding-top"] = " 60px ";
+				        }else if(position==="bottom"){
+				        	page.style["padding-bottom"] = " 60px ";
+				        }  
+    }
+
+}
+window.customElements.define('d-navbar', Navbar);
+
+class Activity extends  HTMLElement {
+    constructor(name,code) {
+        super();
+        this.innerHTML = `<div  class="activity" id="`+name+`"><div class="content"	>`+code+`</div></div>`;
+    }
+    connectedCallback(){  
+        
+    }
+}
+window.customElements.define('d-activity', Activity);
+
+class FloatingButton extends  HTMLElement {
+    constructor() {
+        super();
+    } 
+    connectedCallback(){
+        var button = document.createElement("button");
+        if(this.classList.contains("disabled")===false) {
+            button.setAttribute("class"," ripple FloatingButton "+this.getAttribute("class"));
+        }else{
+            button.setAttribute("class"," FloatingButton "+this.getAttribute("class"));
+        }
+        button.setAttribute("id",this.id);
+        if(this.getAttribute("text")== null){
+            button.innerHTML = ".";
+            button.style = "background-image: url(" + this.getAttribute("icon-src") +") ; background-repeat: no-repeat; background-position: center; font-size: 0;"; 
+        }else{
+            button.innerHTML = this.getAttribute("text");
+            button.style = "background-image: url(" + this.getAttribute("icon-src") +") ; background-repeat: no-repeat; background-position: center; font-size: 15px;"; 
+        }
+        if(this.classList.contains("disabled")=== false) button.setAttribute("onclick",this.getAttribute("onclick"));
+        
+        this.parentElement.parentElement.appendChild(button);    
+        this.remove();
+        console.log(this);   
+        button.addEventListener('click', newRipple); 
+         
+   }
+}
+window.customElements.define('custom-floating-button', FloatingButton);
+
+
+
+function goActivity(act,type){
+
+	document.body.appendChild(act);
+	//document.getElementById(act.children[0].id).classList.add("on_opening");
+	document.getElementById(act.children[0].id).style = "animation: _activity_"+type+" 0.25s;";
+	activitiesHistory.push(act.children[0].id);
+	console.log(activitiesHistory);
+	refreshRippleElements();
+}
+function closeActivity(act,type){
+	if(document.getElementById(act)===null) {
+		error("Tried to close an activity which doesn't exist by the id: '"+act+"'. Tried while being on the activity '"+activitiesHistory[activitiesHistory.length-1]+"'");
+		return;
+	}
+
+	document.getElementById(act).style = " animation: _activity_"+type+" 0.25s;";
+	setTimeout(function(){ 
+		document.getElementById(act).parentElement.remove();
+		activitiesHistory.pop();
+		console.log(activitiesHistory);
+	 }, 250);
+}
+
+function refreshRippleElements(){
+
+var buttons = document.getElementsByClassName('ripple');
+ Array.prototype.forEach.call(buttons, function (b) {
+ 	b.addEventListener('click', newRipple); }); 
+}
+ 
+
+ function newRipple (e) { 
+	 	var circle = document.createElement('div'); 
+	 	this.appendChild(circle);
+	  var d = Math.max(this.clientWidth, this.clientHeight); 
+	  circle.style.width = circle.style.height = d + 'px'; 
+	  var rect = this.getBoundingClientRect(); 
+	  circle.style.left=e.clientX-rect.left-d/2+'px'; 
+	  circle.style.top=e.clientY-rect.top-d/2+'px';
+	  circle.classList = "ripple circle_ripple";
+ };
+
+function error(message){
+	console.error("Dreble > "+message);
+}
+
+const root = document.documentElement;
+var Names = [];
+var PrimaryColors = [];
+var SecondaryColors = [];
+var LightPrimaryColor = [];
+var BackgroundColor = [];
+var RippleEffectColor = [];
+function newTheme($configTheme){
+    Names.push($configTheme["Name"]);
+    PrimaryColors.push($configTheme["Primary"]);
+    SecondaryColors.push($configTheme["Secondary"]);
+    LightPrimaryColor.push($configTheme["Light"]);
+    BackgroundColor.push($configTheme["Background"]);
+    RippleEffectColor.push($configTheme["RippleEffect"]);
+}
+function setTheme(newTheme){
+    switch(newTheme){
+        case "Blue":
+        root.style.setProperty('--PrimaryColor', "#2979ff");
+        root.style.setProperty('--SecondaryColor', "#1565c0");
+        root.style.setProperty('--LightPrimaryColor', "#75a7ff");
+        root.style.setProperty('--BackgroundColor', "white");
+        root.style.setProperty('--RippleEffect', "rgba(255,255,255,0.6)");
+        break;
+        case "Red":
+        root.style.setProperty('--PrimaryColor', "#d50000");
+        root.style.setProperty('--SecondaryColor', "#b71c1c");
+        root.style.setProperty('--LightPrimaryColor', "#ff8a80");
+        root.style.setProperty('--BackgroundColor', "white");
+        root.style.setProperty('--RippleEffect', "rgba(255,255,255,0.6)");
+        break;
+        case "Green":
+        root.style.setProperty('--PrimaryColor', "#00c853");
+        root.style.setProperty('--SecondaryColor', "#43a047");
+        root.style.setProperty('--LightPrimaryColor', "#00e676");
+        root.style.setProperty('--BackgroundColor', "white");
+        root.style.setProperty('--RippleEffect', "rgba(255,255,255,0.6)");
+        break;
+        case "Purple":
+        root.style.setProperty('--PrimaryColor', "#6200ea");
+        root.style.setProperty('--SecondaryColor', "#673ab7");
+        root.style.setProperty('--LightPrimaryColor', "#b388ff");
+        root.style.setProperty('--BackgroundColor', "white");
+        root.style.setProperty('--RippleEffect', "rgba(255,255,255,0.6)");
+        break;
+        case "Orange":
+        root.style.setProperty('--PrimaryColor', "#ff5722");
+        root.style.setProperty('--SecondaryColor', "#e64a19");
+        root.style.setProperty('--LightPrimaryColor', "#ffab91");
+        root.style.setProperty('--BackgroundColor', "white");
+        root.style.setProperty('--RippleEffect', "rgba(255,255,255,0.6)");
+        break;
+        default:
+            for( i = 0; i < PrimaryColors.length; i++){ 
+                if(Names[i] ==newTheme){
+                    root.style.setProperty('--PrimaryColor', PrimaryColors[i]);
+                    root.style.setProperty('--SecondaryColor', SecondaryColors[i]);
+                    root.style.setProperty('--LightPrimaryColor', LightPrimaryColor[i]);
+                    root.style.setProperty('--BackgroundColor', BackgroundColor[i]);
+                    root.style.setProperty('--RippleEffect', RippleEffectColor[i]);
+                }else{
+                    $error("There isn't any theme called < "+newTheme+" >. Define it using 'newTheme()' method.");
+                }
+            }
+    }
+}
+
+function newDialog(activity,code){
+	const page = document.getElementById(activity);
+	const dialog = document.createElement("div");
+	const background = document.createElement("div");
+	const dialog_window = document.createElement("div");
+	const dialog_id = Math.random();
+	dialog_window.classList = "dialog_window";
+	background.classList = "dialog_background";
+	background.setAttribute("onclick","closeDialog(this)")
+	dialog.classList = "dialog";
+	dialog.setAttribute("id",dialog_id); //Assigns a random ID
+	dialog_window.innerHTML += code;
+	dialog_window.setAttribute("toclose",dialog_id); //Assign it's parent ID if they need to remove it
+	background.setAttribute("toclose",dialog_id);	//Assign it's parent ID if they need to remove it
+	dialog.appendChild(background);
+	dialog.appendChild(dialog_window);
+	page.appendChild(dialog);
+}
+function closeDialog(me){
+	document.getElementById(me.getAttribute("toclose")).remove();
+	
+}
+
+
+function Menu(activity,code){
+	const page = document.getElementById(activity);
+	const menu =document.createElement("div");
+	const background = document.createElement("div");
+	const menu_window = document.createElement("div");
+	const menu_id = Math.random();
+	background.classList = "menu_background";
+	background.setAttribute("onclick","closeMenu(this)")
+	menu.classList = "menu";
+	menu.setAttribute("id",menu_id); //Assigns a random ID
+	menu_window.classList = "menu_window";
+	menu_window.innerHTML = code;
+	menu_window.setAttribute("toclose",menu_id);	//Assign it's parent ID if they need to remove it
+	background.setAttribute("toclose",menu_id);	//Assign it's parent ID if they need to remove it
+	menu.appendChild(background);
+	menu.appendChild(menu_window);
+	page.appendChild(menu);
+
+}
+
+function closeMenu(me){
+	const menu = document.getElementById(me.getAttribute("toclose"));
+	menu.children[1].style= "animation:_menu_left 0.2s;";
+
+	setTimeout(function(){ 
+		menu.remove();
+	 }, 200);
+	
+}
---
+++ main.js
@@ -0,0 +1,127 @@
+
+
+const HomeAct = new Activity("Home",`
+
+	<d-navbar activity="Home" position="top">
+		<img class="navbar-icon" onclick="createAppDrawer1()" src="menu.svg"></img>
+		<navbar-title> Main Activity </navbar-title>
+	</d-navbar>
+
+
+	<p>aaaad22</p>
+	<i class="spacer"></i>
+	<button class="ripple" onclick="createDialog1()">Example</button>
+	<p>what's uep</p>
+	<button class="ripple">looool</button>
+	<p>what's up</p>
+	<button class="ripple">Example</button>
+	<p>what's up</p>
+	<button class="ripple">Example</button>
+	<p>what's up</p>
+	<button class="ripple">E2x22ample</button>
+
+	<p>WOW2!</p>
+	<p>AAAA</p>
+	<p>whafft's LOL!!a!</p>
+	<p>what's upj</p>
+	<p>what's up4</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+	<p>what's up</p>
+
+	<p>what's up</p>
+	<p>bbb</p>
+	
+	<button onclick="onActivity2()">config</button>
+	<button onclick="onActivity3()">Counter</button>
+
+	`);
+function createDialog1(){
+	newDialog('Home',`
+
+		<dialog-title>Testing Dialogs</dialog-title>
+		<button class="ripple">Hello!</button>
+
+		`);
+}
+function createAppDrawer1(){
+	new Menu("Home",`
+
+		<button onclick="onActivity2()">Activity 2</button>
+		<p>What's up!!</p>
+
+		`);
+}
+function Main(){
+
+	colors = {
+        Name : 'purple',
+        Primary : 'purple',
+        Light: 'lightpink',
+        Secondary: 'red',
+        Background: 'purple',
+        RippleEffect : 'yellow'
+    }
+    newTheme(colors);
+    setTheme("Blue");	
+
+	const config = {
+		defaultActivity: HomeAct
+	};
+	
+	load(config);
+		 
+}
+
+
+function onActivity2(){
+	const configAct = new Activity("config",`
+	<custom-floating-button icon-src="tick.svg" class=" pos-bottom pos-right  normal" id="fbtn1" onclick="alert('hi!!')"></custom-floating-button>
+
+	<d-navbar activity="config" position="top">
+			<navbar-title> Top bar <navbar-title>
+	</d-navbar>
+
+	<p>aaa</p>
+	<p>config222</p>
+	<p>config</p>
+	<p>config</p>
+	<p>config</p>
+	<p>config</p>
+	<p>config</p>
+	<p>config</p>
+	<p>config</p>
+	<button class="ripple" >example</button>
+	<button class="ripple" onclick="closeActivity('config','slide_right')">back</button>
+	<p>aaa</p>
+	
+	`);
+
+	goActivity(configAct,"slide_left");
+
+}
+
+var _counter = 0;
+function onActivity3(){
+	const counter = new Activity("counter",`
+
+		<custom-floating-button icon-src="plus.svg" class=" pos-bottom pos-center  normal" id="fbtn2" onclick="_counter++; document.getElementById('_counter').innerText='Pressed: '+_counter;"></custom-floating-button>
+
+		<d-navbar activity="counter" position="top">
+			<navbar-title> Top bar <navbar-title>
+		</d-navbar>
+
+		<p id="_counter">Pressed: `+_counter+`</p>
+		<button onclick="closeActivity('counter','slide_down')">Go back</button>
+		<button onclick="onActivity2()">config</button>
+		
+	`);
+	goActivity(counter,"slide_up");
+}
